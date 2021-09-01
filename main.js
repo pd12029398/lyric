04de08632497e785c1673a660f05a31f28a559c8
pd12029398 <84052626+pd12029398@users.noreply.github.com>
2021-08-31 20:34:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var song="u look happy and healthy";
+var song1=document.getElementById("lyric").value;
+function check(){
+
+
+if(song1=="u look happy and healthy"){
+    document.getElementById("output").innerHTML="correct!!";
+}
+}
